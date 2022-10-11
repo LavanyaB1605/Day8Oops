427b50735ca0f2dc06e7025464c88385619baaba
LavanyaB1605 <lavanya.bhaskaran16@gmail.com>
2022-10-11 18:01:37 +0530

    MonthlyWage

---
+++ src/Uc5MonthlyWage.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+public class Uc5MonthlyWage {
+    public static void main(String[] args) {
+
+        Random ec = new Random();
+        int wage = 20;
+        int days = 20;
+        int hour;
+        int salary = 0;
+        int empCheck = ec.nextInt(3);
+
+        if(empCheck == 1) {
+            System.out.println("Employee worked part time");
+            hour = 4;
+            salary = wage * hour * days;
+            System.out.println("Monthly wage of the employee is: " +salary); }
+        else if(empCheck == 2) {
+            System.out.println("Employee worked full time");
+            hour = 8;
+            salary = wage * hour * days;
+            System.out.println("Monthly wage of the employee is: " +salary); }
+        else {
+            System.out.println("Employee is Absent");
+            System.out.println("Monthly wage of the employee is: " +salary);
+        }
+    }
+}
